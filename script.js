fd6391611cc6086be5364b4280542cb5c6163bca
Moulik-6 <193782932+Moulik-6@users.noreply.github.com>
2025-03-29 18:45:38 +0530

    correct logical error in generate computer choice
    check line 9, 10,11

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ function generateHumanChoice(){
 function generateComputerChoice(){
 
     let ran = Math.floor(Math.random() * 10);
-
-    if (1 <= ran <= 3){
+// javascript evalutes operators from left to right so instead of following expression
+// it will evalute it as (1 <= ran) it could return true-1 or false-0 
+// next it checks 1<=3 or 0<=3 which always returns true and excutes only if block
+// so we could write it as (1<=ran ) && (ran<=3)
+    if ((1 <= ran) && (ran <= 3)){
         return "rock";
     } 
-    else if(4 <= ran <= 6){
+    else if((4 <=  ran) && (ran <= 6)){
         return "paper";
     }
     else{
