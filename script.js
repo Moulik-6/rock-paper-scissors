112ab11cea3b46b15fd99028db2fe48a1c7755c6
Moulik-6 <193782932+Moulik-6@users.noreply.github.com>
2025-04-05 20:02:36 +0530

    add the counter for who won

--- script.js
+++ script.js
@@ -13,22 +13,34 @@
 }*/
 
 function generateComputerChoice(){
+    const compChoice = document.querySelector(".comp");
+    const img = document.createElement("img");
+    img.style = "height: 45px; width:45px";
+
 
     let ran = Math.floor(Math.random() * 10);
 
     if ((1 <= ran) && (ran <= 3)){
+        img.src = "./img/rock-icon.png";
+        compChoice.appendChild(img);
         return "rock";
     } 
     else if((4 <=  ran) && (ran <= 6)){
+        img.src = "./img/paper-icon.png";
+        compChoice.appendChild(img);
         return "paper";
     }
     else{
+        img.src = "./img/scissors-icon.png";
+        compChoice.appendChild(img);
         return "scissors";
     }
 
 }
 
 function playRound(humanChoice, computerChoice){
+    const winner = document.querySelector()
+
     if(humanChoice === computerChoice){
         console.log(`Both human and computer played same move ${humanChoice} and its a tie`);
     }
@@ -67,16 +79,26 @@ function compareScores(humanScore, computerScore){
 
 function playGame(){
     const round_no = document.querySelector(".round");
+    const playerChoice = document.querySelector(".player");
+    const img = document.createElement("img");
+    img.style = "height: 45px; width:45px";
+
     round_no.textContent = round++;
     let humanChoice = "";
-    
+
     if(this.id === "r"){
+        img.src = "./img/rock-icon.png";
+        playerChoice.appendChild(img);
         humanChoice = "rock";
     }
     else if(this.id === "p"){
+        img.src = "./img/paper-icon.png";
+        playerChoice.appendChild(img);
         humanChoice = "paper";
     }
     else if(this.id === "s"){
+        img.src = "./img/scissors-icon.png";
+        playerChoice.appendChild(img);
         humanChoice = "scissors";
     }
 
